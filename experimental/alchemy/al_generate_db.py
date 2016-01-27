02f09d84fa8d5fcf0870316a4eefebc5c94383a6
Leopold von Seckendorff <poddus@poddus.com>
2016-01-27 07:46:44 +0100

    uniqueness test not working! hmmmm

--- experimental/alchemy/al_generate_db.py
+++ experimental/alchemy/al_generate_db.py
@@ -2,10 +2,7 @@ from __future__ import print_function, division
 from tabulate import tabulate
 
 from sqlalchemy import create_engine
-engine = create_engine('sqlite:///alchemy.db', echo=False) # echo=True to show SQL statements
-
-# from sqlalchemy.orm import sessionmaker
-# Session = sessionmaker(bind=engine)
+engine = create_engine('sqlite:///alchemy.db', echo=True) # echo=True to show SQL statements
 
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
@@ -56,7 +53,6 @@ Base.metadata.create_all(engine)
 
 """----------------------------------------------------------------------------------------------"""
 """populate table"""
-
 from sqlalchemy.orm import sessionmaker
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -75,7 +71,7 @@ def choose_to_input():
 
 def check_uniqueness(input):
 	for instance in session.query(drink.artNum):
-		if input == instance.artNum:
+		if instance.artNum == input:
 			print("Artikel existiert schon in der Datenbank! Bitte versuchen Sie es erneut.\n\n")
 			return False
 		else:
@@ -96,42 +92,6 @@ def choose_to_accept():
 			print("Bitte entweder 'j' oder 'n' eingeben")
 			continue
 
-while choose_to_input() == True:
-	writeBuffer = []
-	# check for uniqueness of Article Number:
-	# query database for input
-	# if row exists, error
-	# else continue
-	inputArtNum = int(raw_input("Artikelnummer:		"))
-	if check_uniqueness(inputArtNum) == True:
-		writeBuffer.append(inputArtNum)
-	else: continue
-	
-	writeBuffer.append(raw_input("Artikel Name:		"))
-	writeBuffer.append(int(raw_input("Flaschen pro Einheit:	")))
-	writeBuffer.append(int(raw_input("Kasten pro Einheit:	")))
-	writeBuffer.append(float(raw_input("Flaschenpfand:		")))
-	writeBuffer.append(float(raw_input("Einkaufspreis:		")))
-	writeBuffer.append(float(raw_input("Aufschlag pro Flasche:	")))
-	
-	if choose_to_accept() == False:
-		pass
-	else: continue
-
-	if writeBuffer == []:
-		print("Keine Angaben gefunden. Es wurde nichts gemacht")
-	else:
-		session.add(
-			drink(
-				artNum = writeBuffer.pop(0),
-				name = writeBuffer.pop(0),
-				bottlesPerUnit = writeBuffer.pop(0),
-				cratesPerUnit = writeBuffer.pop(0),
-				bottlePfand = writeBuffer.pop(0),
-				unitCost = writeBuffer.pop(0),
-				bottleSurcharge = writeBuffer.pop(0))
-		)
-
 def add_Products():
 	while choose_to_input() == True:
 		writeBuffer = []
@@ -140,7 +100,7 @@ def add_Products():
 		# if row exists, error
 		# else continue
 		inputArtNum = int(raw_input("Artikelnummer:		"))
-		if check_uniqueness(inputArtNum) == True: # this isn't working! wtf
+		if check_uniqueness(inputArtNum) == True:
 			writeBuffer.append(inputArtNum)
 		else: continue
 	
