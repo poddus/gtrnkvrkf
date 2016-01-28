1b0598d05f5f1dd12949801a2106f862ce31cc3b
Leopold von Seckendorff <poddus@poddus.com>
2016-01-28 22:45:27 +0100

    refactored codebase into modules

--- alchemy/add_products.py
+++ alchemy/add_products.py
@@ -1,57 +1,9 @@
-from __future__ import print_function, division
-from tabulate import tabulate
-
 from sqlalchemy import create_engine
 engine = create_engine('sqlite:///alchemy.db', echo=False)    # echo=True to show SQL statements
 
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
-from sqlalchemy import Column, Integer, Float, String, DateTime
-class Drink(Base):
-	"""
-	Common base class for all drinks.
-	A 'unit' is the smallest deliverable unit.
-	'cost' is what we pay.
-	'price' is what the customer pays.
-	"""
-	
-	__tablename__ = "tblProducts"
-	
-	artNum = Column(Integer, primary_key=True)
-	name = Column(String(32))
-	bottlesPerUnit = Column(Integer)
-	cratesPerUnit = Column(Integer)
-	bottlePfand = Column(Float)
-	unitCost = Column(Float)    # pro Liefereinheit, also praktisch pro Kasten
-	bottleSurcharge = Column(Float)
-	
-	def __repr__(self):
-		# print drinks as tabulate table
-		table = []
-		for k in sorted(inventory):    # inventory variable needs to be changed to a database query
-			table.append([self.artNum, self.name, self.get_bottle_price()])
-		return (tabulate(table, headers=["Artikel#", "Name", "Preis Flasche"]))
-	
-	def get_cost_MwSt(self):
-		return self.unitCost*1.19
-	
-	def get_bottle_price(self):
-		return round(((self.get_cost_MwSt() / self.bottlesPerUnit) + 0.1), 2)
-
-class order(Base):
-	
-	__tablename__ = "tblOrder"
-	
-	OrderID = Column(Integer, primary_key=True)
-	timestamp = Column(DateTime)    # how does this one work?
-	note = Column(String)
-	
-
-Base.metadata.create_all(engine)
-
-"""----------------------------------------------------------------------------------------------"""
-"""populate table"""
 from sqlalchemy.orm import sessionmaker
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -90,7 +42,7 @@ def choose_to_accept():
 			print("Bitte entweder 'j' oder 'n' eingeben")
 			continue
 
-def add_Products():
+def add_products():
 	while choose_to_input() is True:
 		writeBuffer = []
 		# check for uniqueness of Article Number:
@@ -102,14 +54,14 @@ def add_Products():
 			writeBuffer.append(inputArtNum)
 		else:
 			continue
-	
+
 		writeBuffer.append(raw_input("Artikel Name:		"))
 		writeBuffer.append(int(raw_input("Flaschen pro Einheit:	")))
 		writeBuffer.append(int(raw_input("Kasten pro Einheit:	")))
 		writeBuffer.append(float(raw_input("Flaschenpfand:		")))
 		writeBuffer.append(float(raw_input("Einkaufspreis:		")))
 		writeBuffer.append(float(raw_input("Aufschlag pro Flasche:	")))
-	
+
 		if choose_to_accept() is True:
 			if writeBuffer == []:
 				print("Keine Angaben gefunden. Es wurde nichts gemacht")
@@ -126,5 +78,6 @@ def add_Products():
 				)
 		else: continue
 
-add_Products()
-session.commit()
+def write_products():
+	add_products()
+	session.commit()
