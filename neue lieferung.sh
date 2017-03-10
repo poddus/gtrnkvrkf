7446760bab49db5f92f96ffc2fc4e5bb8f445315
Leopold Seckendorff <poddus@poddus.com>
2017-03-10 09:13:59 +0100

    Delete neue lieferung.sh

--- neue lieferung.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-## make folder with today's date with "Verkauf" subfolder, create Inventar.ods and open it for editing
-
-cd $( dirname "$0" )
-cp ./template.ods ./buchhaltung/$(date +%F).ods
-libreoffice ./buchhaltung/$(date +%F).ods &
